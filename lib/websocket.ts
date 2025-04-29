01dd2ff7421a82f566be372c867c5cc92fa7415f
denizemekli <denizemekli05@gmail.com>
2025-04-29 17:53:56 +0300

    wss://ringard.com.tr

--- lib/websocket.ts
+++ lib/websocket.ts
@@ -46,7 +46,7 @@ export const useWebSocket = create<WebSocketState>((set: any, get: any) => ({
         globalReconnectTimeout = null;
       }
 
-      const socket = new WebSocket('wss://ws-demo.ringard.net');
+      const socket = new WebSocket('wss://ringard.com.tr');
       globalSocket = socket;
       
       const maxReconnectAttempts = 5;
